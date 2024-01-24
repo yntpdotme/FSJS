bd44c443ef97e2590936d911a3df498d5affaf25
Akash Kadlag <akashkadlag14@gmail.com>
2024-01-24 14:26:53 +0530

    feat: add problem-1 solution

---
+++ JavaScript Questions/Solutions/problem-1.js
@@ -0,0 +1,31 @@
+// 1. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with there type.
+
+let a = "Ram";
+let b = 23;
+let c = 24.44;
+let d = true;
+let e = null;
+let f;
+let g = ["Krishna", "Balram", 12, true];
+
+console.log(`
+value is ${a} and type is ${typeof (a)}
+`)
+console.log(`
+value is ${b} and type is ${typeof (b)}
+`)
+console.log(`
+value is ${c} and type is ${typeof (c)}
+`)
+console.log(`
+value is ${d} and type is ${typeof (d)}
+`)
+console.log(`
+value is ${e} and type is ${typeof (e)}
+`)
+console.log(`
+value is ${f} and type is ${typeof (f)}
+`)
+console.log(`
+value is ${g} and type is ${typeof (g)}
+`)
